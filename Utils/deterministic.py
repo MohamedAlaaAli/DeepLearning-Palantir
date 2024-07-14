c75ad9c6a45b3c7e8dd603d25c5d973a65057586
3luka <94873742+MohamedAlaaAli@users.noreply.github.com>
2024-07-14 10:08:02 +0300

    Update deterministic.py

--- Utils/deterministic.py
+++ Utils/deterministic.py
@@ -1,10 +1,12 @@
 import torch
 import random
 import numpy as np
-seed = 42
-random.seed(seed)
-np.random.seed(seed)
-torch.manual_seed(seed)
-torch.cuda.manual_seed(seed)
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
+
+
+def seed_all(seed:int):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
