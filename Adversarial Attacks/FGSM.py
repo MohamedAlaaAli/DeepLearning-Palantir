69d913502b0912b0e01efd1dab83c003fda10b32
3luka <94873742+MohamedAlaaAli@users.noreply.github.com>
2024-08-02 17:50:44 +0300

    "making the attack targeted"

--- Adversarial Attacks/FGSM.py
+++ Adversarial Attacks/FGSM.py
@@ -3,7 +3,7 @@ import torch
 def fgsm_attack(image, epsilon, gradients):
     
     sign_data_grad = gradients.sign()
-    perturbed_image = image + epsilon * sign_data_grad
+    perturbed_image = image - epsilon * sign_data_grad
     perturbed_image = torch.clamp(perturbed_image, 0, 1)
     
     return perturbed_image
